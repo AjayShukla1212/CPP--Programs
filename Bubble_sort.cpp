5482c6223e92ed2aece7dc7cb0714b1ade9465eb
Ajay Kumar Shukla <sajay6366@gmail.com>
2020-06-19 21:57:25 +0530

    Bubble Sort Uploaeded

---
+++ Bubble_sort.cpp
@@ -0,0 +1,53 @@
+// arr - input array
+// n - size of array
+
+#include <iostream>
+using namespace std;
+
+void BubbleSort(int a[], int n){
+	// Write your code here
+
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int size;
+		cin >> size;
+
+		int *input = new int[size];
+
+		for (int i = 0; i < size; ++i)
+		{
+			cin >> input[i];
+		}
+
+		bubbleSort(input, size);
+
+		for (int i = 0; i < size; ++i)
+		{
+			cout << input[i] << " ";
+		}
+
+		delete[] input;
+		cout << endl;
+	}
+}
