34bafbc22ed14ba0adf0e220891871f4acc23a8e
Ajay Kumar Shukla <sajay6366@gmail.com>
2020-06-20 07:41:08 +0530

    Changes Modified

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int a[], int n){
+void bubbleSort(int *a, int n){
 	// Write your code here
 
     for(int i=0;i<n-1;i++)
@@ -26,22 +26,24 @@ int main()
 {
 
 	int t;
+  cout <<"Enter The Number Of Test Cases : ";
 	cin >> t;
 
 	while (t--)
 	{
 		int size;
+    cout <<"Enter The Size Of Array : ";
 		cin >> size;
 
 		int *input = new int[size];
-
+    cout <<"Arrays Before Bubble Sort : ";
 		for (int i = 0; i < size; ++i)
 		{
 			cin >> input[i];
 		}
 
 		bubbleSort(input, size);
-
+    cout <<"Arrays After Bubble Sort : ";
 		for (int i = 0; i < size; ++i)
 		{
 			cout << input[i] << " ";
